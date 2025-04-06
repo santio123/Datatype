3d8cb7c17bb7496be2277d1a846af3f0aad4527d
santio123 <santioflorin1007@gmail.com>
2025-04-06 10:23:13 +0530

    hi

---
+++ activity3.py
@@ -0,0 +1,28 @@
+str1 = 'Hi'
+str2 = 'World'
+
+# using +
+print('str1+str2 = ', str1 +str2)
+
+str = "string"
+print(str[::3])
+print(str[0::3])
+print(str[::-1])
+
+message = "python is fun"
+print(message.upper())
+
+#input a word
+text =input("Enter a string")
+# Reverse a string
+# using step value as -1 to literate in reverse
+revText = text[::-1]
+text = revText
+
+print("Reverse of the Given string is:")
+print(text)
+
+name = input("Enter your name")
+FirstName = input("Input your firstname")
+LastName = input("input your lastname")
+print('your FullName is', FirstName +" "+ LastName)
