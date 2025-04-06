4b61580885d5d85bdad5113db463ef190e1252b7
santio123 <santioflorin1007@gmail.com>
2025-04-06 09:58:41 +0530

    hi

---
+++ activity2.py
@@ -0,0 +1,13 @@
+name = "santio"
+age=15
+is_student = True
+weight = 38.5
+
+# Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
